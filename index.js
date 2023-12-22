4887f61c78f8d2373541ec32d16a7aae33cc451a
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-22 16:30:36 +0300

    Add edit feature

--- index.js
+++ index.js
@@ -11,19 +11,23 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res)=>{
-    res.render("index.ejs");
+    res.render("index.ejs", {arr:data});
 })
 
 app.post("/",(req, res)=>{
     let user = req.body["user"];
     let text = req.body["text"];
+    console.log("This is text edit: "+textEdit);
     data.push(text);
     console.log(data);
     res.render("index.ejs", {arr:data});
 })
 
 app.get("/blogs",(req,res)=>{
-
+    let lastItem = data.length -1;
+    console.log("last item number: "+lastItem);
+    console.log("last item: "+data[lastItem]);
+    res.render("blogs.ejs",{lastBlog: data[lastItem]})
 })
 
 app.get("/about", (req,res)=>{
@@ -40,3 +44,18 @@ app.post("/blogs", (req,res)=>{
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}.`);
 })
+
+
+// Implement the post creation feature. This includes creating the form on the home page and handling the form submission in the server.
+
+// Implement the post viewing feature. This includes displaying all the posts on the home page.
+
+// Implement the post edit feature. This includes using a form to load the existing blog post and allowing the user to edit and save the post.
+
+// Implement the delete feature. This allows the user to click a button and remove the post from the home page.
+
+// Implement the alternative list to show a different set of todo tasks.
+
+// Test the application to ensure that post creation and viewing are working correctly.
+
+
